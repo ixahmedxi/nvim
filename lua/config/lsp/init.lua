aeccac09f42bfeccdc6241c79b441d4a116ddaa0
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-22 23:49:53 +0100

    chore: format files with stylua

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,125 +1,125 @@
-local lsp_keymaps = require "config.lsp.keymaps"
+local lsp_keymaps = require("config.lsp.keymaps")
 local servers = {
-  "astro",
-  "cssls",
-  "cssmodules_ls",
-  "dockerls",
-  "emmet_ls",
-  "graphql",
-  "html",
-  "jsonls",
-  "tsserver",
-  "sumneko_lua",
-  "marksman",
-  "prismals",
-  "svelte",
-  "tailwindcss",
-  "volar",
-  "yamlls"
+	"astro",
+	"cssls",
+	"cssmodules_ls",
+	"dockerls",
+	"emmet_ls",
+	"graphql",
+	"html",
+	"jsonls",
+	"tsserver",
+	"sumneko_lua",
+	"marksman",
+	"prismals",
+	"svelte",
+	"tailwindcss",
+	"volar",
+	"yamlls",
 }
 
 -- Auto install lsp servers
-require('nvim-lsp-installer').setup({
-  ensure_installed = servers,
+require("nvim-lsp-installer").setup({
+	ensure_installed = servers,
 })
 
 -- Initialise cmp lsp autocompletion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Lsp diagnostics ui customisation
 local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
+	{ name = "DiagnosticSignError", text = "" },
+	{ name = "DiagnosticSignWarn", text = "" },
+	{ name = "DiagnosticSignHint", text = "" },
+	{ name = "DiagnosticSignInfo", text = "" },
 }
 
 for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 local config = {
-  virtual_text = false,
-  signs = {
-    active = signs,
-  },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
+	virtual_text = false,
+	signs = {
+		active = signs,
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 }
 
 vim.diagnostic.config(config)
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
+	border = "rounded",
 })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "rounded",
+	border = "rounded",
 })
 
 -- Default lsp options
 local opts = {
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    if (client.name == 'tsserver') then
-      client.server_capabilities.document_formatting = false
-    end
+	capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		if client.name == "tsserver" then
+			client.server_capabilities.document_formatting = false
+		end
 
-    lsp_keymaps(bufnr)
-  end
+		lsp_keymaps(bufnr)
+	end,
 }
 
 -- Looping all servers and setting them up
 for _, server in ipairs(servers) do
-  -- If a server has custom settings (lua, json) include it in the options
-  local has_custom_opts, custom_opts = pcall(require, "config.lsp.settings." .. server)
+	-- If a server has custom settings (lua, json) include it in the options
+	local has_custom_opts, custom_opts = pcall(require, "config.lsp.settings." .. server)
 
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", custom_opts, opts)
-  end
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", custom_opts, opts)
+	end
 
-  -- Typescript lsp server setup
-  if server == 'tsserver' then
-    require('typescript').setup({
-      disable_commands = false,
-      debug = false,
-      server = opts
-    })
-  else
-    -- Any other lsp server
-    lspconfig[server].setup(opts)
-  end
+	-- Typescript lsp server setup
+	if server == "tsserver" then
+		require("typescript").setup({
+			disable_commands = false,
+			debug = false,
+			server = opts,
+		})
+	else
+		-- Any other lsp server
+		lspconfig[server].setup(opts)
+	end
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
-  sources = {
-    require("null-ls").builtins.diagnostics.eslint_d,
-    require("null-ls").builtins.formatting.prettierd,
-    require("null-ls").builtins.formatting.stylua,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	sources = {
+		require("null-ls").builtins.diagnostics.eslint_d,
+		require("null-ls").builtins.formatting.prettierd,
+		require("null-ls").builtins.formatting.stylua,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,75 +1,75 @@
 local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
-  use "Mofiqul/vscode.nvim"
-  use "nvim-treesitter/nvim-treesitter"
-  use "folke/which-key.nvim"
-  use "kyazdani42/nvim-web-devicons"
-  use "akinsho/bufferline.nvim"
-  use "nvim-lualine/lualine.nvim"
-  use "kyazdani42/nvim-tree.lua"
-  use "nvim-telescope/telescope.nvim"
-  use "goolord/alpha-nvim"
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("Mofiqul/vscode.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use("folke/which-key.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("akinsho/bufferline.nvim")
+	use("nvim-lualine/lualine.nvim")
+	use("kyazdani42/nvim-tree.lua")
+	use("nvim-telescope/telescope.nvim")
+	use("goolord/alpha-nvim")
 
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "saadparwaiz1/cmp_luasnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-  use "windwp/nvim-autopairs"
-  use "numToStr/Comment.nvim"
-  use "JoosepAlviste/nvim-ts-context-commentstring"
-  use "ethanholz/nvim-lastplace"
-  use "lewis6991/impatient.nvim"
-  use "famiu/bufdelete.nvim"
-  use "lukas-reineke/indent-blankline.nvim"
-  use "akinsho/toggleterm.nvim"
-  use "lewis6991/gitsigns.nvim"
-  use "abecodes/tabout.nvim"
-  use "phaazon/hop.nvim"
-  use "tpope/vim-surround"
-  use "aserowy/tmux.nvim"
+	use("windwp/nvim-autopairs")
+	use("numToStr/Comment.nvim")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("ethanholz/nvim-lastplace")
+	use("lewis6991/impatient.nvim")
+	use("famiu/bufdelete.nvim")
+	use("lukas-reineke/indent-blankline.nvim")
+	use("akinsho/toggleterm.nvim")
+	use("lewis6991/gitsigns.nvim")
+	use("abecodes/tabout.nvim")
+	use("phaazon/hop.nvim")
+	use("tpope/vim-surround")
+	use("aserowy/tmux.nvim")
 
-  use "williamboman/nvim-lsp-installer"
-  use "neovim/nvim-lspconfig"
-  use "b0o/schemastore.nvim"
-  use "jose-elias-alvarez/typescript.nvim"
-  use "windwp/nvim-ts-autotag"
-  use "jose-elias-alvarez/null-ls.nvim"
+	use("williamboman/nvim-lsp-installer")
+	use("neovim/nvim-lspconfig")
+	use("b0o/schemastore.nvim")
+	use("jose-elias-alvarez/typescript.nvim")
+	use("windwp/nvim-ts-autotag")
+	use("jose-elias-alvarez/null-ls.nvim")
 
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
