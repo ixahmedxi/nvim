d2fcaacba37d93aebd3187a39a74a802c563c21e
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-26 22:06:51 +0100

    feat: treesitter indent

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -106,7 +106,7 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
 	sources = {
-		require("null-ls").builtins.diagnostics.eslint_d,
+		require("null-ls").builtins.diagnostics.eslint,
 		require("null-ls").builtins.formatting.prettierd,
 		require("null-ls").builtins.formatting.stylua,
 	},
